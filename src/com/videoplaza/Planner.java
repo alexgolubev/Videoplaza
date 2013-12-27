7fa004cf197c8678685b398e0b8ece050184fab1
Alexander Golubev <alexgolubev@padme.belkin>
2013-12-26 19:23:36 -0500

    Initial commit

---
+++ src/com/videoplaza/Planner.java
@@ -0,0 +1,13 @@
+package com.videoplaza;
+
+/**
+ * Main class for campaign selling planning
+ *
+ * @author alexgolubev
+ */
+public class Planner {
+
+    public static void main(String[] pArgs) {
+        System.out.println("Hi there!");
+    }
+}
